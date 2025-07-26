6fb99b012c61159c3bfff100b6f0ec297f4e53d2
xsilentCosmo <xsilentcosmo@gmail.com>
2025-07-26 10:52:23 +0530

    pwa

---
+++ public/sw.js
@@ -0,0 +1,12 @@
+// public/sw.js
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Installed');
+});
+
+self.addEventListener('activate', (event) => {
+  console.log('[Service Worker] Activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
